a5b957b82927c661aeb5837d7829e90237616b16
seabadjo <seartsbadjo225@gmail.com>
2026-05-05 18:34:11 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="min-h-screen flex items-center justify-center bg-black text-white">
+    <h1 className="text-3xl font-bold">Mon site fonctionne !</h1>
+  </div>
+);
+
+export default App;
